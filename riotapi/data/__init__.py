cbf3ef1066fc4be643debd9ac9c5ca6a5d4efda7
Ghost <OverlyGenericAddress@pm.me>
2023-04-02 20:25:43 +0200

    Removed conflicting stuff

--- riotapi/data/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This subpackage holds dataclasses matching the riot games apis responses.
-https://developer.riotgames.com/apis
-
-Product -- GGPT
-https://github.com/Knaeckebrothero/Projekt-GGPT
-App ID -- 616160
-https://developer.riotgames.com/
-"""
